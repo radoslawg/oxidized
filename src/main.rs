44350793ea000848a6f424b7df7591fffd99fb0d
Radoslaw Grzanka <radoslawg@gmail.com>
2026-01-26 23:13:22 +0100

    Basic Hello world Raylib app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use raylib::{self, color::Color, prelude::RaylibDraw};
+
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = raylib::init().size(640, 480).title("Oxidize").build();
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::SKYBLUE);
+        d.draw_text("Hello from Raylib", 10, 200, 40, Color::BLACK);
+        d.draw_fps(5, 5)
+    }
 }
